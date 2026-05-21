3036338a27961a2f1593750cd678aa5a045990f2
kaua alves <kauaalves7163@icloud.com>
2026-05-21 19:21:48 -0300

    passos 6 e 7 cookbook

---
+++ src/main/java/com/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,70 @@
+package com.generation.blogpessoal.model;
+
+import java.time.LocalDateTime;
+
+import org.hibernate.annotations.UpdateTimestamp;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+// Anotaçoes: alterar ou definir comportamentos
+
+@Entity // Definindo que a classe postagem vai se tornar uma tabela
+@Table(name = "tb_postagens") // definir o nome da tabela seguindo a nossa nomeclatura
+public class Postagem {
+
+		@Id // Defini a chave primaria
+		@GeneratedValue(strategy = GenerationType.IDENTITY) // defini que o campo é preenchido pelo banco de dados
+		private Long id;
+			
+		@NotBlank(message = "O atributo título é obrigatório!")
+		@Size(min = 5, max = 100, message = "O atributo título deve ter no minimo 5 e no máximo 100 caracteres.")
+		private String titulo;
+		
+		@NotBlank(message = "O atributo texto é obrigatório!")
+		@Size(min = 10, max = 1000, message = "O atributo texto deve ter no minimo 10 e no máximo 1000 caracteres.")
+		private String texto;
+		
+		@UpdateTimestamp  // bd preenche esse campo e o atualiza
+		private LocalDateTime data;
+
+		public Long getId() {
+			return id;
+		}
+
+		public void setId(Long id) {
+			this.id = id;
+		}
+
+		public String getTitulo() {
+			return titulo;
+		}
+
+		public void setTitulo(String titulo) {
+			this.titulo = titulo;
+		}
+
+		public String getTexto() {
+			return texto;
+		}
+
+		public void setTexto(String texto) {
+			this.texto = texto;
+		}
+
+		public LocalDateTime getData() {
+			return data;
+		}
+
+		public void setData(LocalDateTime data) {
+			this.data = data;
+		}
+		
+	
+	
+}
