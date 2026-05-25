c5843729f568bd81c3c0ef23ba97e3c872a256cf
kaua alves <kauaalves7163@icloud.com>
2026-05-25 15:47:43 -0300

    cookbook 13 e 14 atualizado

--- src/main/java/com/generation/blogpessoal/controller/PostagemController.java
+++ src/main/java/com/generation/blogpessoal/controller/PostagemController.java
@@ -20,6 +20,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import com.generation.blogpessoal.model.Postagem;
 import com.generation.blogpessoal.repository.PostagemRepository;
+import com.generation.blogpessoal.repository.TemaRepository;
 
 import jakarta.validation.Valid;
 
@@ -30,6 +31,9 @@ public class PostagemController {
 
 	@Autowired // inversao de controle
 	private PostagemRepository postagemRepository;
+	
+	@Autowired
+	private TemaRepository temaRepository;
 	
 	@GetMapping // todas as requisiçoes do tipo get vai ser executadas por esse metodo
 	public ResponseEntity<List<Postagem>> getAll(){ // get all vai buscar todas as postagens, e vai ser retornado um lista de postagens
@@ -51,19 +55,31 @@ public class PostagemController {
 
      @PostMapping
  	 public ResponseEntity<Postagem> post(@Valid @RequestBody Postagem postagem) {
+    	 
+    	 if (temaRepository.existsById(postagem.getTema().getId())) {
  		
  		postagem.setId(null);
  		
  		return ResponseEntity.status(HttpStatus.CREATED)
  				.body(postagemRepository.save(postagem));
+    	 }
+    	 
+    	 throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema não existe!", null);
  	}
      
     @PutMapping
  	public ResponseEntity<Postagem> put(@Valid @RequestBody Postagem postagem) {
- 		return postagemRepository.findById(postagem.getId())
- 				.map(resposta -> ResponseEntity.status(HttpStatus.OK)
- 						.body(postagemRepository.save(postagem)))
- 				.orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+    	if (postagemRepository.existsById(postagem.getId())) {
+    		
+    		if (temaRepository.existsById(postagem.getTema().getId()))
+    			return ResponseEntity.status(HttpStatus.OK)
+    					.body(postagemRepository.save(postagem));
+    		
+    		throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Tema não existe!", null);
+    		
+    	}
+    					
+ 		return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
  	}
  	
     @ResponseStatus(HttpStatus.NO_CONTENT)
